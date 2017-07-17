6321697465d841c0bf8427e3e8f4e5d48f8b8867
Boba Festival <fbcindykathleen@gmail.com>
2017-07-17 08:15:50 -0700

    Contact Form
    Testing index.php

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-
-    $from = 'From: Boba Festival Contact Page'; 
+    $from = 'From: TheBobaFestival'; 
     $to = 'cindy.kathleen.wang@gmail.com'; 
-    $subject = 'Message';
-
+    $subject = 'New Message';
+    $human = $_POST['human'];
+			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-    if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) { 
+				
+    if ($_POST['submit']) 
+    {				 
+        if (mail ($to, $subject, $body, $from)) 
+        { 
             echo '<p>Your message has been sent!</p>';
-        } else { 
-            echo '<p>Something went wrong, please go back and try again!</p>'; 
-        }
+	    } 
+        else 
+        { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+	    } 
     }
 ?>
